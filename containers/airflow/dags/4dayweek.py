455f4d661c0230fc6c034db610a98cd23e620764
dematom1 <laszlo@hey.com>
2024-01-29 15:20:11 -0700

    update env airflow

---
+++ containers/airflow/dags/4dayweek.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+}
+
+def fetch_data():
+    ''''
+    Fetchs data from Data First Jobs
+    '''
+    pass
+
+def query_external_api_and_transform(**kwargs):
+    # Fetch required data (from XComs or intermediate storage)
+    # Make request to external API
+    # Transform data
+    # Return transformed data
+    pass
+
+def post_data_to_api(**kwargs):
+    # Fetch transformed data (from XComs or intermediate storage)
+    # Make POST request to your API
+    pass
+
+with DAG(dag_id='4dayweek_api_pull', default_args=default_args, start_date=datetime(2024, 1, 30), schedule="0 0 * *") as dag:
+    pass
