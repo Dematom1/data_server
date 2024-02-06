3e85c24a2b35336aafd5a3642ecb2423adad8011
dematom1 <laszlo@hey.com>
2024-02-05 21:34:16 -0700

    remove --no-cache

--- server_deploy.sh
+++ server_deploy.sh
@@ -5,7 +5,7 @@ echo "Deploying application...."
 git pull 
 docker-compose down 
 echo "docker down"
-docker-compose build --no-cache
+# docker-compose build --no-cache
 docker-compose up -d
 echo "docker up"
 
